dd179e264ffc5e09fca16355bb44bef05caba006
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-26 15:25:05 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import *
+from pyrogram import *
+
+
+Star_msg = "Hellow World"
