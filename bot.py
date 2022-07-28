77dbed96c2b039d94b95d9425b200dbed50b0592
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-28 17:47:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,11 @@ from pyrogram import errors
 from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 
 from youtubesearchpython import VideosSearch
+import re
+import json
 
+import requests
+from bs4 import BeautifulSoup
 
 bot = Client(
     "EpSong Bot",
@@ -169,7 +173,30 @@ async def inline(client: Client, query: InlineQuery):
 
 #---------------------------Song Bot Epic-------------------------------------#
 #-------------------Epic-------------------------------------#
-
+@bot.on_message(filters.command('lyric'))
+def LyricWikia(artist, title):
+    url = 'http://lyrics.wikia.com/api.php?action=lyrics&artist={artist}&song={title}&fmt=json&func=getSong'.format(
+        artist=artist,
+        title=title).replace(" ", "%20")
+    r = requests.get(url)
+    # We got some bad formatted JSON data... So we need to fix stuff :/
+    returned = r.text
+    returned = returned.replace('"', "")
+    returned = returned.replace("\'", "\"")
+    returned = returned.replace("song = ", "")
+    returned = json.loads(returned)
+    if returned["lyrics"] != "Not found":
+        # set the url to the url we just recieved, and retrieving it
+        r = requests.get(returned["url"])
+        soup = BeautifulSoup(r.text, 'html.parser')
+        soup = soup.find("div", {"class": "lyricbox"})
+        [elem.extract() for elem in soup.findAll('div')]
+        [elem.replaceWith('\n') for elem in soup.findAll('br')]
+        soup = BeautifulSoup(re.sub(r'(<!--[.\s\S]*-->)', '', str(soup)), 'html.parser')
+        [elem.extract() for elem in soup.findAll('script')]
+        return soup.getText()
+    else:
+        return None
 
 print (f"""
 #╔════╗────────╔═══╗
