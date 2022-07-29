a1233e8b338fcf01df2b72c7ff7d0e15de214ff5
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-07-29 07:13:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -180,47 +180,57 @@ async def inline(client: Client, query: InlineQuery):
 
 #---------------------------Song Bot Epic-------------------------------------#
 #-------------------Epic-------------------------------------#
-api_key = "JVv8pud-25QRBYyRwcH34AlAygySsSAU3owRNGBw6hXO96x0JiTMn-3R4PvsjcTf"
-y = lg.Genius(
-    api_key,
-    skip_non_songs=True,
-    excluded_terms=["(Remix)", "(Live)"],
-    remove_section_headers=True,
-)
-y.verbose = False
+import os
+import aiofiles
+import aiohttp
+from pyrogram import filters
+
+ARQ = "https://thearq.tech/"
+
+CHABEE = Client
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
+
+async def download_song(url):
+    song_name = f"Chabee.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
 
 
-lyrical = {}
-
-LYRICS_COMMAND = ("LYRICS_COMMAND")
-
-@bot.on_message(filters.command(LYRICS_COMMAND))
-async def lrsearch(client, message: Message, _):
+@CHABEE.on_message(filters.command("deezer"))
+async def deezer(_, message):
     if len(message.command) < 2:
-        return await message.reply_text(_["lyrics_1"])
-    title = message.text.split(None, 1)[1]
-    m = await message.reply_text(_["lyrics_2"])
-    S = y.search_song(title, get_full_info=False)
-    if S is None:
-        return await m.edit(_["lyrics_3"].format(title))
-    ran_hash = "".join(
-        random.choices(string.ascii_uppercase + string.digits, k=10)
-    )
-    lyric = S.lyrics
-    if "Embed" in lyric:
-        lyric = re.sub(r"\d*Embed", "", lyric)
-    lyrical[ran_hash] = lyric
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["L_B_1"],
-                    url=f"https://t.me/EpSongBot?start=lyrics_{ran_hash}",
-                ),
-            ]
-        ]
-    )
-    await m.edit(_["lyrics_4"], reply_markup=upl)
+        await message.reply_text("Download Now Deezer")
+        return
+    text = message.text.split(None, 1)[1]
+    query = text.replace(" ", "%20")
+    m = await message.reply_text("Searching...")
+    try:
+        r = await fetch(f"{ARQ}deezer?query={query}&count=1")
+        title = r[0]["title"]
+        url = r[0]["url"]
+        artist = r[0]["artist"]
+    except Exception as e:
+        await m.edit(str(e))
+        return
+    await m.edit("Downloading...")
+    song = await download_song(url)
+    await m.edit("Uploading...")
+    await message.reply_audio(audio=song, title=title, performer=artist)
+    os.remove(song)
+    await m.delete()
 
 
 
