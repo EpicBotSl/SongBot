5b544f2c269bc6b244eb7649d4a9a20c0730f419
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-11 17:41:49 +0530

    Update arq.py

--- utils/arq.py
+++ utils/arq.py
@@ -26,3 +26,13 @@ async def post(url: str, *args, **kwargs):
         except Exception:
             data = await resp.text()
     return data
+
+
+BASE = "https://batbin.me/"
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
