975647a0a0ff40f955a5f3add623a0bbc2f7f226
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-11 17:39:42 +0530

    Update arq.py

--- utils/arq.py
+++ utils/arq.py
@@ -5,6 +5,9 @@ from pyrogram.types import Message
 from pyromod import listen
 from Python_ARQ import ARQ
 
+from asyncio import gather
+
+
 ARQ_API_URL = "https://thearq.tech"
 
 ARQ_API_KEY = "JRBVAR-JICHKN-DFLDNX-NPRGCH-ARQ"
@@ -13,3 +16,13 @@ ARQ_API_KEY = "JRBVAR-JICHKN-DFLDNX-NPRGCH-ARQ"
 aiohttpsession = ClientSession()
 
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+
+
+
+async def post(url: str, *args, **kwargs):
+    async with session.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
