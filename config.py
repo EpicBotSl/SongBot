b113b96b0df15adbdfb37cf45901fe2e16c3d333
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-12 15:40:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,8 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+AUTH_CHATS = os.getenv("AUTH_CHATS", "").strip()
 
-LOG_GROUP = "-1001620454933"
-LOGGER = "-1001620454933"
 
 #commands
 
