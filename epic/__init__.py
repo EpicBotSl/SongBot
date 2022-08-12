a64875b27f32a5fc66686cc9142a86aa61fa4df5
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-12 09:50:16 +0530

    Update __init__.py

--- epic/__init__.py
+++ epic/__init__.py
@@ -54,6 +54,8 @@ class bot(Client):
         LOGGER.info("Bot Stopped, Bye.")
 
 #---------------------------Gen Logo Epic-------------------------------------#
+#aiogram installation
+
 API_TOKEN = 'BOT_TOKEN'
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -62,41 +64,4 @@ shazam = Shazam()
 
 #&-&-&-&-&-&-&-&-&-&-&-&-&-&-&-&-&-&-&---&-#
 
-
-    async def start(self):
-        await super().start()
-        print("bot started. Hi.")
-
-    async def stop(self, *args):
-        await super().stop()
-        print("bot stopped. Bye.")
-
-    async def recognize(self, path):
-        return await shazam.recognize_song(path)
-
-    async def related(self, track_id):
-        try:
-            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
-        except exceptions.FailedDecodeJson:
-            return None
-    
-    async def get_artist(self, query: str):
-        artists = await shazam.search_artist(query=query, limit=50)
-        hits = []
-        try:
-            for artist in artists['artists']['hits']:
-                hits.append(FactoryArtist(artist).serializer())
-            return hits
-        except KeyError:
-            return None
-        
-    async def get_artist_tracks(self, artist_id: int):
-        tracks = []
-        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
-        try:
-            for track in tem:
-                tracks.append(FactoryTrack(data=track).serializer())
-            return tracks
-        except KeyError:
-            return None
 
