f8616cbeb8315e9101fbfeefd88fef86eab4b06e
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-12 10:42:03 +0530

    Update __main__.py

--- epic/__main__.py
+++ epic/__main__.py
@@ -1,14 +1,8 @@
 from epic import bot
 from os import sys,mkdir,path
-from aiogram import executor
-from epic import dp
+
 
 if __name__ == "__main__":
     if not path.exists("cache"):
         mkdir("cache")
     bot().run()
-
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
