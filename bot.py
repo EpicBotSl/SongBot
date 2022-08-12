e9567e59cd3e2ae2362fc9826d1243ce7bf8f204
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-12 05:37:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,64 @@
-import config 
-from config import *
-import logging
-from pyrogram import Client, idle
-from pyromod import listen  # type: ignore
-from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+from os import path
+from configparser import ConfigParser
+from pyrogram import Client
+from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
 
-logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+shazam = Shazam()
 
-bot = Client(
-    "Epic Developers",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    in_memory=True,
-    plugins=dict(root="module"),
-)
+class bot(Client):
+    def __init__(self, name):
+        config_file = f"{name}.ini"
+        config = ConfigParser()
+        config.read(config_file)
+        name = name.lower()
+        plugins = {'root': path.join(__package__, 'plugins')}
+        api_id = config.get('pyrogram', 'api_id')
+        api_hash = config.get('pyrogram', 'api_hash')
+        super().__init__(
+            name,
+            api_id=api_id,
+            api_hash=api_hash,
+            config_file=config_file,
+            workers=16,
+            plugins=plugins,
+            workdir="./",
+        )
 
+#---------------------------Gen Logo Epic-------------------------------------#
 
-if __name__ == "__main__":
-    print("⌛Updating & Analyzing bot⏳")
-    try:
-        bot.start()
-    except (ApiIdInvalid, ApiIdPublishedFlood):
-        raise Exception("Your API_ID/API_HASH is not valid.")
-    except AccessTokenInvalid:
-        raise Exception("Your BOT_TOKEN is not valid.")
-    uname = bot.get_me().username
-    print(f"""@{uname} is Accepted
-╔════╗────────╔═══╗
-║╔╗╔╗║────────║╔══╝
-╚╝║║╠╩═╦══╦╗╔╗║╚══╦══╦╦══╗
-──║║║║═╣╔╗║╚╝║║╔══╣╔╗╠╣╔═╝
-──║║║║═╣╔╗║║║║║╚══╣╚╝║║╚═╗
-──╚╝╚══╩╝╚╩╩╩╝╚═══╣╔═╩╩══╝
-──────────────────║║
-──────────────────╚╝
-Join For update @EpicBotsSl""")
-    idle()
-    bot.stop()
-    print("Bot stopped. Alvida!")
+    async def start(self):
+        await super().start()
+        print("bot started. Hi.")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("bot stopped. Bye.")
+
+    async def recognize(self, path):
+        return await shazam.recognize_song(path)
+
+    async def related(self, track_id):
+        try:
+            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
+        except exceptions.FailedDecodeJson:
+            return None
+    
+    async def get_artist(self, query: str):
+        artists = await shazam.search_artist(query=query, limit=50)
+        hits = []
+        try:
+            for artist in artists['artists']['hits']:
+                hits.append(FactoryArtist(artist).serializer())
+            return hits
+        except KeyError:
+            return None
+        
+    async def get_artist_tracks(self, artist_id: int):
+        tracks = []
+        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
+        try:
+            for track in tem:
+                tracks.append(FactoryTrack(data=track).serializer())
+            return tracks
+        except KeyError:
+            return None
