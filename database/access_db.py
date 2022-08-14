2fb3130ea2991b4457fb9f4823ce100c5846f573
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-14 13:46:40 +0530

    Update access_db.py

--- database/access_db.py
+++ database/access_db.py
@@ -1,4 +1,4 @@
 from config import *
 from database.database import Database
 
-db = Database(DATABASE_URL, "spot_dl")
+db = Database(mongodb+srv://epic:epic@cluster0.257eaxu.mongodb.net/?retryWrites=true&w=majority, "spot_dl")
